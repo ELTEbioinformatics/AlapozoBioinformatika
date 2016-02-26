c412904a44a3351f7df4ad1a6f1630a91caa2781
nazgul <tamas.kadlecsik@tgac.ac.uk>
2016-02-26 11:57:31 +0100

    correction

--- 3_file_handling/scripts/write_to_file.R
+++ 3_file_handling/scripts/write_to_file.R
@@ -1,14 +1,14 @@
 # Same as before, the file needs to be read
 # Change the path according to your needs.
 people <- read.table("C:/Users/nazgul/Desktop/toy.csv", header = T, sep=',')
-age <- people$age
-age.sum <- sum(age)
-age.mean <- mean(age)
-age.median <- median(age)
+height <- people$height
+height.sum <- sum(height)
+height.mean <- mean(height)
+height.median <- median(height)
 
 # Here is a good use of a list. When we want to group different variables
 # and even name them, list comes to our aid.
-stats <- list(median=age.median, mean=age.mean, sum= age.sum)
+stats <- list(median=height.median, mean=height.mean, sum= height.sum)
 
 # This is the way we write files. See the handout for further details.
 # For even further details see "?write.table"
